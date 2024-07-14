2c132a8872eedd296f387d5bb33f7f47e567dee7
Luigi Uchiyama <luigi_uchiyama@outlook.com>
2024-07-13 21:24:43 -0300

    Burguer Mania

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.module.css';
+import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Routes/Home';
 import Header from './General/Header';
